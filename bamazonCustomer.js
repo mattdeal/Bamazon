f388d249f633f4ac3b5b2a162038cf48eefa61ae
mwdeal1 <mwdeal1@gmail.com>
2017-03-01 18:41:56 -0500

    customer todo code

---
+++ bamazonCustomer.js
@@ -0,0 +1,8 @@
+// todo: main menu
+// - show all products
+// - prompt for product id, quantity
+// - - if ID AND Quantity, place order else show error and return to main menu
+
+// todo: purchaseItem(id, qty)
+// - decrease item.quantity by qty
+// - if success, show user receipt
