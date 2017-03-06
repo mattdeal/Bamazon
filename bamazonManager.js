8952fc450492a02e3eb86f917455a3088b3f127c
mwdeal1 <mwdeal1@gmail.com>
2017-03-06 16:05:48 -0500

    Implement Bamazon Manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,37 +9,155 @@ var connection = mysql.createConnection({
 	database: 'Bamazon'
 });
 
+var QUERY_ALL_PRODUCTS = 'SELECT id, name, price, stock_quantity FROM products';
+var QUERY_LOW_PRODUCTS = 'SELECT id, name, price, stock_quantity FROM products WHERE stock_quantity < 5';
+var QUERY_MODIFY_STOCK = 'UPDATE products SET stock_quantity = ? WHERE id = ?';
+var QUERY_ADD_PRODUCT = 'INSERT INTO products SET ?';
+
 // connect to db
 connection.connect(function(err) {
 	if (err) throw err;
 	// console.log('connected as ' + connection.threadId);
 });
 
+// display options to user and take appropriate action based on selection
 function mainMenu() {
-    // todo: show options
+    inquirer.prompt([  
+        {
+            type: "list",
+            name: "mode",
+            message: "What would you like to do?",
+            choices: ["View All Products", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
+        }
+    ]).then(function(selection) {
+        switch(selection.mode) {
+            case 'View All Products':
+                viewProducts(QUERY_ALL_PRODUCTS, mainMenu);
+                break;
+            case 'View Low Inventory':
+                viewProducts(QUERY_LOW_PRODUCTS, mainMenu);
+                break;
+            case 'Add to Inventory':
+                viewProducts(QUERY_ALL_PRODUCTS, modifyInventory);
+                break;
+            case 'Add New Product':
+                viewProducts(QUERY_ALL_PRODUCTS, addProduct);
+                break;
+            case 'Exit':
+                connection.end();
+                return;
+            default:
+                console.log('invalid');
+                return;
+        }
+    });
 }
 
-function viewProducts() {
-    // todo: show id, name, price, qty
-    // todo: return to main menu
+// show inventory levels and run callback function
+function viewProducts(query, callback) {
+    connection.query(query, function(err, res) {
+		if (err) throw err;
+
+        // show inventory
+		console.table(res);
+        console.log('----------------------------------------');
+
+        if (callback && callback !== null) {
+            callback();
+        }
+    });
 }
 
-function viewLowInventory() {
-    // todo: show id, name, price, qty where qty < 5
-    // todo: return to main menu
-}
-
+// prompt user for the product to modify
 function modifyInventory() {
-    // todo: add to current inventory
-    // todo: display updated inventory for updated product
-    // todo: return to main menu
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the ID of the item you wish to update",
+            name: "id",
+            validate: function(str) {
+                return !isNaN(parseInt(str));
+            }
+        },
+        {
+            type: "input",
+            message: "Enter the new quantity",
+            name: "stock_quantity",
+            validate: function(str) {
+                return !isNaN(parseInt(str));
+            }
+        }
+    ]).then(function(product) {
+        modifyProduct(product);
+    });
+}
+
+// update the product in the database
+function modifyProduct(product) {
+    console.log(product);
+
+    connection.query(QUERY_MODIFY_STOCK, [product.stock_quantity, product.id], function(err, res) {
+		if (err) throw err;
+
+        // show inventory
+        console.log('----------------------------------------');     
+		console.log('Product Updated!');
+        console.log('----------------------------------------');     
+
+        mainMenu();  
+    });
 }
 
 function addProduct() {
-    // todo: prompt for values
-    // todo: add to db
-    // todo: show result
-    // todo: return to main menu
+    // prompt for values
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the product name",
+            name: "name",
+            validate: function(str) {
+                return str.length > 0;
+            }
+        },
+        {
+            type: "input",
+            message: "Enter the product department",
+            name: "department_name",
+            validate: function(str) {
+                return str.length > 0;
+            }
+        },
+        {
+            type: "input",
+            message: "Enter the price",
+            name: "price",
+            validate: function(str) {
+                return !isNaN(parseInt(str));
+            }
+        },
+        {
+            type: "input",
+            message: "Enter the quantity",
+            name: "stock_quantity",
+            validate: function(str) {
+                return !isNaN(parseInt(str));
+            }
+        }
+    ]).then(function(product) {
+        console.log(product);
+
+        // add to db
+        connection.query(QUERY_ADD_PRODUCT, [product], function(err, res) {
+            if (err) throw err;
+
+            // show inventory
+            console.log('----------------------------------------');     
+            console.log('Product Added!');
+            console.log('----------------------------------------');     
+
+            mainMenu();  
+        });
+    });
 }
 
 // start the app
